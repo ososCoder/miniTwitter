08cb6ec2988d1380e0a17f3d000b2fa4987d1b4a
oscar_ousinde <oscar.ousinde@hotmail.com>
2023-02-05 16:56:08 +0100

    Starting register user

--- frontend/src/pages/RegisterPage/RegisterPage.js
+++ frontend/src/pages/RegisterPage/RegisterPage.js
@@ -1,8 +1,58 @@
+import { useState } from 'react';
+
 export const RegisterPage = () => {
+  const [email, setEmail] = useState('');
+  const [pass1, setPass1] = useState('');
+  const [pass2, setPass2] = useState('');
+  const [error, setError] = useState('');
+
+  const handleForm = async (e) => {
+    e.preventDefault();
+    setError('');
+
+    if (pass1 !== pass2) {
+      setError('Passwords do not match');
+      return;
+    }
+  };
+
   return (
     <section>
       <h2>Register</h2>
-      <p>Formulario de registro</p>
+      <form onSubmit={handleForm}>
+        <fieldset>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </fieldset>
+        <fieldset>
+          <label htmlFor='pass1'>Password</label>
+          <input
+            type='password'
+            id='pass1'
+            name='pass1'
+            required
+            onChange={(e) => setPass1(e.target.value)}
+          ></input>
+        </fieldset>
+        <fieldset>
+          <label htmlFor='pass2'>Repeat Password</label>
+          <input
+            type='password'
+            id='pass2'
+            name='pass2'
+            required
+            onChange={(e) => setPass2(e.target.value)}
+          ></input>
+        </fieldset>
+        <button>Register</button>
+        {error ? <p>{error}</p> : null}
+      </form>
     </section>
   );
 };
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -21,3 +21,19 @@ export const getSingleTweetService = async (id) => {
 
   return json.data;
 };
+
+export const registerUserService = async ({ email, password }) => {
+  const response = await fetch(`${process.env.REACT_APP_BACKEND}/user`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw new Error(json.message);
+  }
+};
